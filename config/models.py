a2f91fb89ccf3e95707cdf75059f54161bb5092b
r-sato <ryh-sato@chieru-cb.co.jp>
2025-05-03 18:07:24 +0900

    修正

---
+++ config/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Record(models.Model):
+    name = models.CharField(max_length=30)
+    win_streak = models.IntegerField()
+    played_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.win_streak}連勝 ({self.played_at:%Y-%m-%d %H:%M})" 
--- config/views.py
+++ config/views.py
@@ -1,5 +1,28 @@
 # config/views.py
 from django.shortcuts import render
+import random
 
 def home(request):
-    return render(request, "home.html")
+    result = None
+    player_choice = None
+    computer_choice = None
+    
+    if request.method == 'POST':
+        player_choice = request.POST.get('choice')
+        choices = ['グー', 'チョキ', 'パー']
+        computer_choice = random.choice(choices)
+        
+        if player_choice == computer_choice:
+            result = '引き分け'
+        elif (player_choice == 'グー' and computer_choice == 'チョキ') or \
+             (player_choice == 'チョキ' and computer_choice == 'パー') or \
+             (player_choice == 'パー' and computer_choice == 'グー'):
+            result = '勝ち'
+        else:
+            result = '負け'
+    
+    return render(request, 'home.html', {
+        'result': result,
+        'player_choice': player_choice,
+        'computer_choice': computer_choice
+    })
